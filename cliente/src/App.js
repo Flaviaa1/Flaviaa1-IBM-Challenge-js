1a76305180eaf2ad99ff605421fd1d754bdbf9bc
Flavia Gregorio <flaviagregorio262@gmail.com>
2021-02-26 14:48:55 -0300

    Enviando el token via context al back

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,16 +1,31 @@
 import React, { Component, Fragment } from 'react';
 import { ApolloProvider } from "react-apollo";
 import {BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
-import ApolloClient from "apollo-boost";
+import ApolloClient, {InMemoryCache} from "apollo-boost";
 import Header from './componentes/Header'
 import Ibm from './componentes/Ibm'
 import NuevoIbm from './componentes/NuevoIbm';
-import  EditarIbm from './componentes/EditarIbm';
+import EditarIbm from './componentes/EditarIbm';
 import Registro from './componentes/Auth/Registro';
 import Login from './componentes/Auth/Login'
 const client = new ApolloClient({
 
   uri: "http://localhost:4000/graphql",
+  // enviar token
+   fetchOptions: {
+   credentials: 'include'
+   },
+   request: operation => {
+     const token = localStorage.getItem('token')
+     operation.setContext({
+       headers:{
+         authorization: token
+       }
+     })
+   },
+  cache: new InMemoryCache({
+     addTypename:false
+  }),
   
   onError: ({networkError, graphQLErrors}) => {
     console.log('networkErrors', graphQLErrors);
